39e6ddd8f43e8d31f8d0d5d09e861674a6e30c62
OPTIMISSA\carlos.cueva <Carlos>
2022-10-13 15:43:51 +0200

    Final

--- carro-de-compras/src/App.js
+++ carro-de-compras/src/App.js
@@ -12,16 +12,53 @@ class App extends Component {
       {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
       {name: 'Guisante', price: 2500, img: '/productos/guisante.jpg'},
       {name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},      
-    ]
+    ],
+    carro: [
+
+    ],
+    esCarroVisible: false,
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name 
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+
+      })
+      : x)
+      return this.setState({ carro:newCarro})
+    }
+
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if(!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible})
+  }
+
   render() {
+    const { esCarroVisible } = this.state
     return(
       <div>
-        <NavBar/>
-        <Title/>
+        <NavBar 
+          carro = {this.state.carro} 
+          esCarroVisible={esCarroVisible} 
+          mostrarCarro={this.mostrarCarro}/>
         <Layout>
+          <Title/>
           <Productos
-            agregarAlcarro={() => console.log('No hace nada')}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
